f8643263d4d898dda29a3c874a3b196ac90bcdf5
Timur Khvat <timurkhvat@gmail.com>
2025-05-20 15:54:16 +0200

    'Solution'

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,3 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, generics, mixins, viewsets
 from django.shortcuts import get_object_or_404
